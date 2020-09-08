e77c6bae5590326895c847f556a050e013cee3a6
Volodymyr Vuchkan <vvv.jsx@gmail.com>
2020-09-08 19:55:36 +0300

    Delete file logger

--- src/Bot.js
+++ src/Bot.js
@@ -11,55 +11,51 @@ class Bot {
         this.bot = new TelegramBot(token, {polling: true});
 
         this.bot.on('message', async (msg) => {
-            log.info(`
-user: ${msg.chat.first_name}(@${msg.chat.username})
-text: ${msg.text}
-`)
 
             const url = parsing.getLink(msg.text);
-            log.info(`url: ${url}`)
 
             if(!url) {
-                this.bot.sendMessage(msg.chat.id, 'Link not found')
-                this.bot.sendMessage(ADMIN_CHAT_ID, `
+                const log = `
 [user]: ${msg.chat.first_name}(@${msg.chat.username})
 [msg.text]: ${msg.text} 
 [url:] ${url}
-`);
+`;
+                this.bot.sendMessage(ADMIN_CHAT_ID, log);
+                this.bot.sendMessage(msg.chat.id, 'Link not found')
                 return 0;
             }
 
             const description = await parsing.grabSongDescription(url);
-            log.info(`description: ${description}`)
 
             const query = parsing.extractSongInfo(description);
-            log.info(`query: ${query}`)
             const searchResult = await Lyrics.search(encodeURI(query));
             const lyrics = await parsing.grabLyrics(searchResult.url);
-            log.info(`lyrics.length: ${lyrics.length}`)
             const text = lyrics || searchResult.lyrics || '';
 
-            this.bot.sendMessage(ADMIN_CHAT_ID, `
+            const log = `
 [user]: ${msg.chat.first_name}(@${msg.chat.username})
 [msg.text]: ${msg.text} 
 [url:] ${url}
 [description]: ${description} 
 [query]: ${query}
-[lyrics.length]: ${text.length}`);
+[lyrics.length]: ${text.length}
+`;
+            this.bot.sendMessage(ADMIN_CHAT_ID, log);
 
             if (text.length === 0) {
                 this.bot.sendMessage(msg.chat.id, 'Oops, something went wrong')
                 return 0;
             }
+
             if (text.length < 4096) {
                 this.bot.sendMessage(msg.chat.id, text)
-            } else {
-                const chunks = text.match(/(.|[\r\n]){1,4095}/g);
-                chunks.map(async chunk => {
-                    await this.bot.sendMessage(msg.chat.id, chunk)
-                })
+                return 0;
             }
 
+            const chunks = text.match(/(.|[\r\n]){1,4095}/g);
+            chunks.map(async chunk => {
+                await this.bot.sendMessage(msg.chat.id, chunk)
+            })
         });
     }
 }
